4ae764231975182e7cffaf37eb1d904bc70a40fb
Midhun <midhunskani@gmail.com>
2024-08-31 13:42:13 +0530

    Basic CRUD using fastapi

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import psycopg2
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,6 +14,12 @@ except psycopg2.Error as e:
     print(e)
 
 
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -22,8 +29,47 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
 @app.get("/posts")
 async def posts():
     cursor.execute("""SELECT * FROM posts""")
     posts = cursor.fetchall()
     return posts
+
+
+@app.post("/posts", status_code=201)
+async def create_post(post: PostBase):
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"message": new_post}
+
+
+@app.get("/posts/{id}")
+async def get_post(id: int):
+    cursor.execute("""SELECT * FROM posts WHERE ID = %s""", (str(id),))
+    post = cursor.fetchone()
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
+
+@app.delete("/posts/{id}", status_code=204)
+async def delete_post(id: int):
+    cursor.execute("""DELETE FROM posts WHERE ID = %s RETURNING *""", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+
+@app.put("/posts/{id}", status_code=200)
+async def update_post(id: int, post: PostBase):
+    cursor.execute("""UPDATE posts SET title= %s, content = %s, published=%s  WHERE id = %s RETURNING *""",
+                   (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return {"data": updated_post}
